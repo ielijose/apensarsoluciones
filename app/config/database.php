f35b288c14b756d06decb8254b7e81b35ca55e19
Eli José Carrasquero [Laptop] <ielijose@gmail.com>
2015-05-09 11:28:22 -0430

    Forge

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'apensarsoluciones',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'apensar',
+			'username'  => 'forge',
+			'password'  => 'aIxQq4AdfmvibVD3VeR2',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
